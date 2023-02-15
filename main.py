1e5d4bfc033936dfc4cbe3d7bd3d174acf9e956b
VadimD <dlyanoutano8@gmail.com>
2023-02-15 15:21:29 +0300

    ВЕРНУЛИ НА СВОИ МЕСТА!

--- main.py
+++ main.py
@@ -1,14 +1,2 @@
-from fastapi import FastAPI
-
-
-import app as app
-import motor.motor_asyncio
-
-
-
-client = motor.motor_asyncio.AsyncIOMotorClient
-("mongodb://Aboba:89210408@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
-
-@app.get("/myget")
-async def get_int(user_id: int):
-    return user_id
+a = input("Введите пожалуйста, дорогой пользователь нашего приложения букву А! Она будет использована для дальнейших манипуляций в сфере программироваия. Ваш выбор вы можете указать тут: ")
+print(f"Вы ввели данную переменную: {a}")
