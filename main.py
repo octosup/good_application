670e33ce1068a850b952d49edfc180c61387b494
VadimD <dlyanoutano8@gmail.com>
2023-02-15 15:10:00 +0300

    commit

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-a = input("Введите пожалуйста, дорогой пользователь нашего приложения букву А! Она будет использована для дальнейших манипуляций в сфере программироваия. Ваш выбор вы можете указать тут: ")
-print(f"Вы ввели данную переменную: {a}")
+from fastapi import FastAPI
+
+
+import app as app
+import motor.motor_asyncio
+
+
+
+client = motor.motor_asyncio.AsyncIOMotorClient
+("mongodb://Aboba:89210408@127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
+
+@app.get("/myget")
+async def get_int(user_id: int):
+    return user_id
